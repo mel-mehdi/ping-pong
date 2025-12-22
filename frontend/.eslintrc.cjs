5af439060c899b248fac4fceac971e3ae1c6be4f
mel-medhi <mehdimahfoud321@gmail.com>
2025-12-22 10:59:59 +0100

    chore(frontend): add ESLint CJS config and Prettier

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
